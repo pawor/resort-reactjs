12b9d168102d8a7e69a1f3de59b2384605ce86b0
pawor <pawor.emi@gmail.com>
2021-09-23 22:57:49 +0700

    create pages

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>hello from home page</div>
+    );
+}
+export default Home
---
+++ src/pages/Rooms.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>hello from room page</div>
+    );
+}
+export default Rooms
